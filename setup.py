6580bbbae602d7989b57f609d63a59ad22882546
Qi Sun <qisun.career@gmail.com>
2019-03-17 14:12:07 -0400

    Update on 2019/03/17 2:12PM
    Same as the last one

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+try:
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except IOError:
+    long_description = 'Make it easier for you to experiment with quantitative ideas.'
 
 setup(
     name='quant_experiment',
     version='1.0.0',
     description='Python module to get stock and option information through Wed Scraping and Alpha Vantage API',
+    long_description=long_description,
     url='https://github.com/liveoncecao/quant_experiment',
     author='Quentin Sun',
     author_email='qsun.career@gmail.com',
@@ -24,5 +34,5 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     keywords=['stocks', 'market', 'options', 'pricing', 'quant', 'quotes', 'tickers', 'symbol'],
-    install_requires=['requests', 'math', 'pandas', 'beautifulsoup4'],
+    install_requires=['requests', 'pandas', 'beautifulsoup4'],
 )
