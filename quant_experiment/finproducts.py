4bd6ff3a490477ac098b4027513c5181576a3c5f
Qi Sun <qisun.career@gmail.com>
2019-04-04 16:15:01 -0400

    Added test_quant_experiment

--- quant_experiment/finproducts.py
+++ quant_experiment/finproducts.py
@@ -9,14 +9,14 @@ from .mathformulas import riskfree
 
 
 class VanillaOption:
-    def __init__(self, symbol, d, m, y, strike, type):
+    def __init__(self, symbol, d, m, y, strike, opt_type):
         """
         :param symbol: e.g. 'AAPL', 'MSFT'
         :param d: e.g. 1,2,3,...,31
         :param m: e.g. 1,2,....,12
         :param y: e.g. 2018,2019
         :param strike: e.g. 100,105
-        :param type: e.g. 'calls' or 'puts'
+        :param opt_type: e.g. 'calls' or 'puts'
         """
 
         self._strike = strike
@@ -29,7 +29,8 @@ class VanillaOption:
         if len(self._response) == 0:
             raise LookupError("Could not corresponding information for the symbol.")
 
-        self._expirationDates = [datetime.utcfromtimestamp(i).date() for i in self._response[0]['expirationDates']]
+        self._expirationDates = [datetime.utcfromtimestamp(
+            i).date() for i in self._response[0]['expirationDates']]
         self._strikes = self._response[0]['strikes']
 
         # Raise error when could not find information for relevant expiration date or strike price
@@ -39,7 +40,7 @@ class VanillaOption:
             raise LookupError("Strike prices are: ", self._strikes)
 
         # Raise error when returned value is empty because sometimes there could be only call or put
-        self._type = type.lower()
+        self._type = opt_type.lower()
         if self._type == 'calls' and len(self._response[0]['options'][0]['calls']) == 0:
             raise LookupError('No such call exists.')
         if self._type == 'puts' and len(self._response[0]['options'][0]['puts']) == 0:
@@ -62,18 +63,18 @@ class VanillaOption:
         """
         return self._option_info
 
-    def BS_Info(self,key=None,info_name='implied_vol',q=0):
+    def BS_Info(self, key=None, info_name='implied_vol', q=0):
         """
         Based on Black Scholes model, calculated implied vol will lose its accuracy for DeepInTheMoney
         and DeepOutOfMoney options with relatively short time to maturity, use with your own discretion
         """
-        stock_info = Stock(self._symbol,key)
+        stock_info = Stock(self._symbol, key)
         self._stock_price = stock_info.price
         self._tau = (self._date - self._last_trade_date).days/365
         self._r = riskfree()(self._tau)
 
-        self.BandS = BlackandScholes(self._stock_price,self._strike,self._tau,self._r,
-                                      self._option_info['lastPrice'][0],self._type,q)
+        self.BandS = BlackandScholes(self._stock_price, self._strike, self._tau, self._r,
+                                     self._option_info['lastPrice'][0], self._type, q)
 
         if info_name == 'implied_vol':
             return self.BandS.imp_vol
@@ -84,14 +85,14 @@ class VanillaOption:
 
 
 class Stock:
-    def __init__(self,symbol,key=None):
+    def __init__(self, symbol, key=None):
         url = "{}function={}".format(_ALPHA_VANTAGE_API_URL, 'GlOBAL_QUOTE')
         url = '{}&{}={}'.format(url, 'symbol', symbol.upper())
         url = '{}&apikey={}'.format(url, key)
         response = list(requests.get(url).json()['Global Quote'].values())
         self._open, self._high, self._low, self._price,\
-        self._volume, self._latestday , self._prevclose , self._change, \
-        self._changepercent = response[1:]
+            self._volume, self._latestday, self._prevclose, self._change, \
+            self._changepercent = response[1:]
 
     @property
     def open(self):
@@ -128,5 +129,3 @@ class Stock:
     @property
     def changePercent(self):
         return float(self._changepercent)
-
-
